91460aa282796aa9259840a62b46a21504b69b89
David <yoxisem544@gmail.com>
2018-06-25 22:25:55 +0800

    - setup project

---
+++ Example/Pods/Target Support Files/ElegantMoya/ElegantMoya-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ElegantMoya : NSObject
+@end
+@implementation PodsDummy_ElegantMoya
+@end
---
+++ Example/Pods/Target Support Files/Pods-ElegantMoya_Example/Pods-ElegantMoya_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ElegantMoya_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ElegantMoya_Example
+@end
